0c21ec492c9f56f5c79afc05f74e67dbb630610b
flowfelis <alicandonmez90@gmail.com>
2020-12-27 19:33:28 +0100

    Added, "getting visitors based on date" endpoint.

--- app/visitor.py
+++ app/visitor.py
@@ -4,6 +4,7 @@ from models import UniqueVisitor
 from app import app
 from sqlalchemy.exc import SQLAlchemyError
 from flask import jsonify
+from schemas import unique_visitor_schema
 
 
 @app.route('/unique_visitor/<int:visitor_id>', methods=['POST'])
@@ -21,17 +22,20 @@ def add_unique_visitor(visitor_id):
     try:
         db.session.add(visitor)
         db.session.commit()
-        return jsonify(), 201
+        # return jsonify(), 201
+        return unique_visitor_schema.jsonify(visitor), 201
     except SQLAlchemyError:
         db.session.rollback()
-        return jsonify(), 500
+        return jsonify({'error': 'An error occurred'}), 500
 
 
-@app.route('/unique_visitors')
-def get_all_unique_visitors():
+@app.route('/unique_visitors/<string:date>')
+def get_all_unique_visitors(date):
     """
     GET Daily visitors- how many unique visitors did the site see on a specific day.
     """
-    visitors = UniqueVisitor.query.all()
+    visitors = UniqueVisitor.query.filter_by(visit_date=date).all()
+    # import pdb
+    # pdb.set_trace()
 
-    return jsonify()
+    return unique_visitor_schema.jsonify(visitors, many=True)
